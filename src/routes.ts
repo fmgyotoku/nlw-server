266a6276534cb3294db25d6cb907623ac577fcf4
Fernando M Gyotoku <fernando.gyotoku@gmail.com>
2023-04-14 08:00:38 +1000

    Summary list route creation

--- src/routes.ts
+++ src/routes.ts
@@ -78,7 +78,93 @@ export async function appRoutes(app: FastifyInstance) {
     }
   })
 
+  /**
+   * Toggle method to update the information of a habit which has been completed /
+   * incompleted
+   * @param id the unique identifier of the activity
+   * @param date the date when the activity is being completed
+   */
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid()
+    })
+
+    const toggleHabitQueryParams = z.object({
+      date: z.coerce.date()
+    })
+
+    const { id } = toggleHabitParams.parse(request.params)
+    const { date } = toggleHabitQueryParams.parse(request.query)
+    const targetDate = dayjs(date).startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: { 
+        date: targetDate
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: targetDate
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id, 
+          habit_id: id
+        }
+      }
+    })
+
+    if(dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id, 
+          habit_id: id
+        }
+      })
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            cast(count(1) as float)
+          FROM day_habits DH 
+          WHERE DH.day_id = D.id
+        ) as completed, 
+        (
+          SELECT 
+            cast(count(1) as float)
+          FROM 
+            habit_days_of_week HDOW
+          JOIN habits H
+            ON H.id = HDOW.habit_id
+          WHERE 
+            HDOW.day_of_week = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `
+
+    return summary
+  })
+
   app.get('/', () => {
-    return 'Hello! =)'
+    return 'Hello! =)\nIf you reach this point, your Node JS instance is working fine!'
   })
 }
